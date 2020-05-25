d31fac1935fd72db6cb916b8a444c58e9a503732
deanbgarlick <deanbgarlick@gmail.com>
2020-05-25 19:50:20 +0100

    Improve interface

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -3,20 +3,22 @@ import time
 
 class Cell:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, x_lim, y_lim):
 
         self.x = x
         self.y = y
         self.status = 'dead'
-        self.adjacent_cells_list = self._get_adjacent_cells_list()
+        self.adjacent_cells_list = self._get_adjacent_cells_list(x_lim, y_lim)
         self.next_status = ''
 
-    def _get_adjacent_cells_list(self):
+    def _get_adjacent_cells_list(self, x_lim, y_lim):
         adjacent_cells_list = []
         for x_increment in [-1, 0, 1]:
             for y_increment in [-1, 0, 1]:
                 if x_increment!=0 and y_increment!=0:
-                    adjacent_cells_list.append((self.x + x_increment, self.y + y_increment))
+                    if (self.x + x_increment < x_lim) and (self.x + x_increment > 0):
+                        if (self.y + y_increment < y_lim) and (self.y + y_increment > 0):
+                            adjacent_cells_list.append((self.x + x_increment, self.y + y_increment))
         return adjacent_cells_list
 
 
@@ -26,7 +28,7 @@ class Matrix:
 
         self.x_lim = x_lim
         self.y_lim = y_lim
-        self.cell_grid = [(Cell(x, y) for x in range(x_lim)) for y in range(y_lim)]
+        self.cell_grid = [[Cell(x, y, x_lim, y_lim) for x in range(x_lim)] for y in range(y_lim)]
 
     def get_cell_from_coordinate(self, cell_coordinate):
         return self.cell_grid[cell_coordinate[0]][cell_coordinate[1]]
@@ -36,7 +38,7 @@ class Matrix:
 
     def count_alive_adjacent_cells(self, cell_coordinate):
         count = 0
-        for cell_coordinate in self.cell_grid[cell_coordinate[0]][cell_coordinate[1]].adjacent_cells_list:
+        for cell_coordinate in self.get_cell_from_coordinate(cell_coordinate).adjacent_cells_list:
             if self.check_status_of_cell(cell_coordinate) == 'alive':
                 count += 1
         return count
@@ -88,9 +90,10 @@ class Interface:
 
     def play_conways_game_of_life(self, n_epocs, alive_coordinates_list, time_interval=4):
         self.matrix.set_initial_state(alive_coordinates_list)
-        for _ in range(1, n_epocs+1):
+        self.view.print_matrix_state(self.matrix, 0)
+        for epoc in range(1, n_epocs+1):
             self.matrix.run_for_n_epocs(1, time_interval)
-            self.view.print_matrix_state(self.matrix)
+            self.view.print_matrix_state(self.matrix, epoc)
 
 
 class View:
@@ -101,11 +104,16 @@ class View:
     def set_display_tokens(self, alive_token, dead_token):
         self.display_token_dict = {'alive': alive_token, 'dead': dead_token}
 
-    def print_matrix_state(self, matrix):
-        row_divider = '|-'*len(matrix[0]) + '|'
-        for cell_row in matrix:
-            print(row_divider)
+    def print_matrix_state(self, matrix, epoc):
+        top_row = ['|'+str(n) for n, _ in enumerate(matrix.cell_grid[0])]
+        top_row = ''.join(top_row)
+        top_row = '-' + top_row + '|'
+        row_divider = '|-'*len(matrix.cell_grid[0]) + '|'
+        print('\n\n\n Epoc: '+str(epoc) + '\n')
+        print(top_row)
+        print(row_divider)
+        for rowno, cell_row in enumerate(matrix.cell_grid):
             cell_row_view = ['|'+str(self.display_token_dict[cell.status]) for cell in cell_row]
             cell_row_view = ''.join(cell_row_view) + '|'
-            print(cell_row_view)
-            print(row_divider)
+            print(str(rowno) + cell_row_view)
+            print('-'+row_divider)
---
+++ main.py
@@ -0,0 +1,4 @@
+from game_of_life import game_of_life
+
+interface = game_of_life.Interface(9, 9)
+interface.play_conways_game_of_life(3, [(3, 4), (4, 4), (4, 5)])
